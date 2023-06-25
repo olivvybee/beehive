0d878759e93639326c6a74ff6678270a62101fe1
Olivia Asch <olivia.c.asch@gmail.com>
2023-06-25 16:55:29 +0100

    Add a custom 404 page

---
+++ app/not-found.tsx
@@ -0,0 +1,17 @@
+import Link from 'next/link';
+import styles from './not-found.module.css';
+
+const NotFound = () => (
+  <>
+    <h2>Not found</h2>
+
+    <p>That page doesn't seem to exist.</p>
+
+    <p>
+      Try going back to the <Link href="/">homepage</Link>, or the list of{' '}
+      <Link href="/blog">blog posts</Link>.
+    </p>
+  </>
+);
+
+export default NotFound;
