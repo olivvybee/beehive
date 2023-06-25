e38c3bdb59a4341700147f8ff11e80b12c3b053a
Olivia Asch <olivia.c.asch@gmail.com>
2023-06-25 16:57:24 +0100

    Add placeholders for /code and /crafts

---
+++ app/code/page.tsx
@@ -0,0 +1,11 @@
+const CodePage = () => (
+  <>
+    <h2>Code</h2>
+    <p>
+      Eventually this page will describe some of the code-related things I get
+      up to.
+    </p>
+  </>
+);
+
+export default CodePage;
---
+++ app/crafts/page.tsx
@@ -0,0 +1,11 @@
+const CraftsPage = () => (
+  <>
+    <h2>Crafts</h2>
+    <p>
+      Eventually this page will describe some of the crafting-related things I
+      get up to.
+    </p>
+  </>
+);
+
+export default CraftsPage;
