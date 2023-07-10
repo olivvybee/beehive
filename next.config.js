0ffa73c032d03f85c6e09da4f1d4766781c59e24
Olivia Asch <olivia.c.asch@gmail.com>
2023-07-10 19:34:30 +0100

    Set up redirects for old url scheme

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  redirects: async () => [
+    {
+      source: '/:year(\\d{1,})/:month(\\d{1,})/:day(\\d{1,})/:slug',
+      destination: '/blog/:year/:month/:day/:slug',
+      permanent: true,
+    },
+  ],
+};
 
 module.exports = nextConfig;
