91e324843f35d0abd22c0e2ff2e7570e668275cd
Liv Asch <olivia.c.asch@gmail.com>
2023-07-11 13:33:57 +0100

    Update pronouns

--- app/page.tsx
+++ app/page.tsx
@@ -19,7 +19,7 @@ const Homepage = () => (
       </p>
       <p>
         I'm <a href="https://www.lgbtqia.wiki/wiki/Agender">agender</a> and my
-        pronouns are <strong>she/her</strong>. I tend to refer to myself as a
+        pronouns are <strong>they/she</strong>. I tend to refer to myself as a
         trans woman, although it's a bit more complicated than that.
       </p>
     </InlineImage>
