dcace854b55babef3e288efb1d3648386f141fbd
Liv Asch <olivia.c.asch@gmail.com>
2023-12-29 11:16:46 +0000

    Small tweaks to the homepage

--- app/page.tsx
+++ app/page.tsx
@@ -17,14 +17,14 @@ const Homepage = () => (
         as olivvybee.
       </p>
       <p>
-        I'm <a href="https://www.lgbtqia.wiki/wiki/Agender">agender</a> and my
+        I'm <a href="https://new.lgbtqia.wiki/wiki/Agender">agender</a> and my
         pronouns are <strong>she/they</strong>. I tend to refer to myself as a
         trans woman, although it's a bit more complicated than that.
       </p>
     </InlineImage>
     <p>
-      I'm a software engineer from the south east of the UK. Currently I'm
-      working as a senior frontend engineer.
+      I'm a software engineer from the south east of the UK. I mostly focus on
+      frontend development now, but I started out as a full stack developer.
     </p>
     <p>
       Previously I was a full stack engineer at the Lego Group, specifically
@@ -36,20 +36,20 @@ const Homepage = () => (
     <p>
       Most of my programming is done in <a href="https://reactjs.org">React</a>{' '}
       and <a href="https://www.typescriptlang.org">Typescript</a>, both at work
-      and at home. I'm an aspiring iOS developer too, although I've not managed
-      to make anything of note yet - but watch this space! Take a look at the{' '}
+      and at home. I'm an aspiring iOS developer too, although I've not made
+      much progress yet - but watch this space! Take a look at the{' '}
       <Link href="/code">code page</Link> to see what I've been working on.
     </p>
     <p>
-      Away from the code, I like to cross stitch, 3D print, bake, and play games
-      (video and board). Some examples are on the{' '}
+      Away from the code, I like to cross stitch, 3D print, bake, and play board
+      and video games. Some things I've made are on the{' '}
       <Link href="/crafts">crafts page</Link>.
     </p>
     <p>
       Online, the best place to find me to see what I'm up to or have a chat is
       on the fediverse, where I'm{' '}
       <a href="https://fedi.beehive.gay/@olivvybee" rel="me">
-        @olivvybee@fedi.beehive.gay
+        @olivvybee@beehive.gay
       </a>
       . Other, more specific places are listed on the{' '}
       <Link href="/links">links page</Link>.
