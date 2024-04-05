c1f8a0d593960abd2a109736f19b16f7ea00d093
Liv Asch <olivia.c.asch@gmail.com>
2024-04-05 10:14:29 +0100

    Add implementation details for train map

--- app/trains/page.tsx
+++ app/trains/page.tsx
@@ -40,6 +40,40 @@ const TrainsPage = () => {
         knowing which parts of the network I have used and where the tracks are
         on a map.
       </p>
+
+      <h3 id="implementation">How I implemented this</h3>
+
+      <p>
+        For each route, I use{' '}
+        <a href="https://brouter.de/brouter-web/#map=17/standard&profile=rail">
+          BRouter
+        </a>{' '}
+        to draw the route and export it as a GeoJSON file. Those files are then
+        grouped by operator in the filesystem, and there's code to read all the
+        files from those directories and parse the JSON.
+      </p>
+      <p>
+        The map itself is rendered using a React wrapper around{' '}
+        <a href="https://maplibre.org/maplibre-gl-js/docs/">
+          <code>maplibre-gl</code>
+        </a>
+        . Each of the parsed GeoJSON files is turned into a layer on the map,
+        colour coded by operator (the colours are defined separately, not in the
+        JSON).
+      </p>
+      <p>
+        When you select an operator, all the other layers are removed from the
+        map. Then the minimum and maximum longitudes and latitudes are
+        calculated for the remaining routes and the map is recentred using those
+        coordinates, so that the routes are nicely framed in the viewport.
+      </p>
+      <p>
+        The code is available{' '}
+        <a href="https://github.com/olivvybee/beehive/tree/main/app/trains">
+          on github
+        </a>{' '}
+        if you're interested in the fine details.
+      </p>
     </TrainsMapContextProvider>
   );
 };
