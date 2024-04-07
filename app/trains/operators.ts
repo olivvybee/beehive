bfd847aa50734ce3ff3a85b044c5c286129a18c1
Liv Asch <olivia.c.asch@gmail.com>
2024-04-07 22:01:09 +0100

    Add heritage railways

--- app/trains/operators.ts
+++ app/trains/operators.ts
@@ -5,6 +5,7 @@ export interface Operator {
 }
 
 export const OPERATORS: Operator[] = [
+  { id: 'bluebell', name: 'Bluebell Railway', colour: 'rgb(155, 177, 235)' },
   { id: 'dlr', name: 'DLR', colour: 'rgb(77, 169, 167)' },
   { id: 'edi-trams', name: 'Edinburgh Trams', colour: 'rgb(128, 35, 27)' },
   { id: 'elizabeth', name: 'Elizabeth Line', colour: 'rgb(105, 80, 161)' },
@@ -12,6 +13,7 @@ export const OPERATORS: Operator[] = [
   { id: 'northern', name: 'Northern', colour: 'rgb(64, 59, 163)' },
   { id: 'overground', name: 'London Overground', colour: 'rgb(246, 128, 39)' },
   { id: 'renfe', name: 'Renfe', colour: 'rgb(118, 21, 91)' },
+  { id: 'rhdr', name: 'RH&DR', colour: 'rgb(91, 130, 62)' },
   { id: 'southern', name: 'Southern', colour: 'rgb(56, 120, 95)' },
   { id: 'thameslink', name: 'Thameslink', colour: 'rgb(207, 49, 131)' },
 ];
