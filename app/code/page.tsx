efd5adc3cd7889a63e472baa2baed539ce09345e
Liv Asch <olivia.c.asch@gmail.com>
2024-04-08 14:25:09 +0100

    Link to train map from code page

--- app/code/page.tsx
+++ app/code/page.tsx
@@ -1,3 +1,4 @@
+import { Link } from '@/components/Link';
 import { buildMetadata } from '@/utils/metadata';
 
 export const metadata = buildMetadata({
@@ -10,9 +11,14 @@ const CodePage = () => (
   <>
     <h2>Code</h2>
     <p>
-      Eventually this page will describe some of the code-related things I get
-      up to.
+      These are some code projects I work on or have worked on in my spare time.
     </p>
+    <p>
+      My <Link href="/trains">interactive train map</Link> shows all the train
+      lines I have travelled on.
+    </p>
+    <hr />
+    <p>More coming soon...</p>
   </>
 );
 
