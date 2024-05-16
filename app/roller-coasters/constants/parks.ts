fecf625e650299270b59cd3c77aff79f353e8347
Liv Asch <olivia.c.asch@gmail.com>
2024-05-16 11:35:31 +0100

    Add alton towers

--- app/roller-coasters/constants/parks.ts
+++ app/roller-coasters/constants/parks.ts
@@ -3,6 +3,7 @@ import { Park } from '../types';
 export const PARKS: Park[] = [
   { id: 'thorpe-park', name: 'Thorpe Park' },
   { id: 'chessington', name: 'Chessington World of Adventures' },
+  { id: 'alton-towers', name: 'Alton Towers' },
 ];
 
 export const UNKNOWN_PARK: Park = { id: 'xxx', name: 'Unknown' };
