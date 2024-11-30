19a76dd1e0f695d995c5a01a2ccbc315a79b8cf3
Liv Flowers <oliviaflowers93@gmail.com>
2024-11-30 16:43:09 +0000

    Add back link to coasters update page

--- app/roller-coasters/update/[parkId]/page.tsx
+++ app/roller-coasters/update/[parkId]/page.tsx
@@ -1,8 +1,12 @@
 import { notFound } from 'next/navigation';
+import { IoChevronBack } from 'react-icons/io5';
+
+import { Link } from '@/components/Link';
 
 import { getPark } from '../../api';
 
 import { Editor } from './Editor';
+import styles from './page.module.css';
 
 interface CoasterListPageProps {
   params: {
@@ -20,6 +24,10 @@ const CoasterListPage = async ({ params }: CoasterListPageProps) => {
 
   return (
     <>
+      <Link href="/roller-coasters/update" className={styles.backLink}>
+        <IoChevronBack /> Back to park list
+      </Link>
+
       <h2>Update coasters at {park.name}</h2>
 
       <Editor coasters={park.coasters} />
