eacd11ac65ad3d6cc8d1c4a33b159f469681c1d8
Liv Flowers <oliviaflowers93@gmail.com>
2025-03-17 21:52:25 +0000

    Remove asides on coasters and trains

--- app/coasters/page.tsx
+++ app/coasters/page.tsx
@@ -63,42 +63,6 @@ const RollerCoasters = async () => {
             },
           ]}
         />
-
-        <aside>
-          <h3 id="implementation">How I implemented this</h3>
-
-          <p>
-            For each coaster, I find its data on{' '}
-            <a href="https://rcdb.com">RCDB</a> and save it in a{' '}
-            <a href="https://developers.cloudflare.com/d1/">
-              Cloudflare D1 database
-            </a>{' '}
-            along with data about the theme park it resides in. The page then
-            loads all the coasters from the database to display in the list or
-            on the map.
-          </p>
-          <p>
-            The map itself is rendered using a React wrapper around{' '}
-            <a href="https://maplibre.org/maplibre-gl-js/docs/">
-              <code>maplibre-gl</code>
-            </a>
-            . Each coaster from the database is turned into a marker on the map
-            with a popup containing the data.
-          </p>
-          <p>
-            When you select a group or park, the minimum and maximum longitudes
-            and latitudes are calculated for the coasters in that view and the
-            map is recentred using those coordinates, so that they are nicely
-            framed in the viewport.
-          </p>
-          <p>
-            The code is available{' '}
-            <a href="https://github.com/olivvybee/beehive/tree/main/app/coasters">
-              on github
-            </a>{' '}
-            if you're interested in the fine details.
-          </p>
-        </aside>
       </div>
     </>
   );
