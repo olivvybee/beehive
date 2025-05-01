d811a1e0b1f04a5993b8fec803b48d7a13f8dc80
Liv Flowers <oliviaflowers93@gmail.com>
2025-05-01 13:58:10 +0100

    Move button definitions to separate json file

--- components/ButtonGrid/ButtonGrid.tsx
+++ components/ButtonGrid/ButtonGrid.tsx
@@ -1,5 +1,7 @@
 import classNames from 'classnames';
 
+import buttons from './buttons.json';
+
 import styles from './ButtonGrid.module.css';
 
 interface Button {
@@ -10,87 +12,6 @@ interface Button {
   vertical?: boolean;
 }
 
-const BUTTONS: Button[] = [
-  {
-    src: '/button.png',
-    alt: 'A bee and the text "beehive.gay" with a rainbow underline.',
-    title: 'beehive.gay',
-  },
-  {
-    src: 'https://theresnotime.co.uk/button.png',
-    alt: 'A fox and the text "TheresNoTime" on a subtle non-binary flag background.',
-    href: 'https://theresnotime.co.uk',
-    title: 'TheresNoTime (Sammy)',
-  },
-  {
-    src: '/buttons/honeycomb-engineer.png',
-    alt: 'A rainbow beehive and the text "honeycomb.engineer".',
-    href: 'https://honeycomb.engineer',
-    title: 'honeycomb.engineer',
-  },
-  {
-    src: 'https://k4m1.net/button.gif',
-    alt: 'K4M1 meows @ you',
-    href: 'https://k4m1.net/',
-    title: 'K4M1',
-  },
-  {
-    src: 'https://taavi.wtf/img/buttons/me/taavi.png',
-    alt: 'Taavi',
-    href: 'https://taavi.wtf/',
-    title: 'Taavi',
-  },
-  {
-    src: 'https://flufftech.net/button.png',
-    alt: 'The word "Rail" and a fox on a non-binary flag background.',
-    href: 'https://flufftech.net/',
-    title: 'Rail',
-  },
-  {
-    src: 'https://david.garden/wp-content/uploads/2024/09/david-site-button.png',
-    alt: '"david.garden" in the middle in pink, a plant on the left, and a dog emoji with pink hair on the right',
-    href: 'https://david.garden/',
-    title: 'david wolfpaw',
-  },
-  {
-    src: 'https://enikofox.com/enikodoesbadthingstocode.png',
-    alt: '"Eniko does bad things to code" and a girl with blurple hair.',
-    href: 'https://enikofox.com',
-    title: 'Eniko Fox',
-  },
-  {
-    src: '/buttons/lf.png',
-    alt: 'QR code registration marks with LF in the middle. Below is the text "enter the next line" and a hand drawn laptop with a winking face made from a greater than sign and an underscore.',
-    href: 'https://lf-net.org',
-    title: 'Littlefox',
-    vertical: true,
-  },
-  {
-    src: 'https://symtrkl.gay/img/88x31/symtrkl-dot-gay.png',
-    alt: '"SymTrkl dot Gay" written on top of a light pink to light blue to light pink gradient.',
-    href: 'https://symtrkl.gay',
-    title: 'SymTrkl',
-  },
-  {
-    src: 'https://liv.town/assets/img/livtown-button.gif',
-    alt: '"liv.town" on a pale purple background with animated rainbow confetti in the background.',
-    href: 'https://liv.town',
-    title: 'liv.town',
-  },
-  {
-    src: 'https://vulpinecitrus.info/vcinfo-online.png',
-    alt: '"VCInfo" surrounded by pixel art of foxes and a lemon.',
-    href: 'https://vulpinecitrus.info/',
-    title: 'Vulpine Citrus',
-  },
-  {
-    src: 'https://girlthi.ng/~thermia/img/88x31/thermia.gif',
-    alt: '"~thermia" surrounded by PCB traces. It\'s animated and lights up letter by letter.',
-    href: 'https://girlthi.ng/~thermia/',
-    title: 'thermia',
-  },
-];
-
 interface ButtonGridProps {
   id?: string;
 }
@@ -98,14 +19,18 @@ interface ButtonGridProps {
 export const ButtonGrid = ({ id }: ButtonGridProps) => (
   <div id={id} className={styles.buttonGrid}>
     <div className={styles.horizontalButtons}>
-      {BUTTONS.filter((button) => !button.vertical).map((button) => (
-        <ButtonGridItem key={button.title} button={button} />
-      ))}
+      {buttons
+        .filter((button) => !button.vertical)
+        .map((button) => (
+          <ButtonGridItem key={button.title} button={button} />
+        ))}
     </div>
     <div className={styles.verticalButtons}>
-      {BUTTONS.filter((button) => button.vertical).map((button) => (
-        <ButtonGridItem key={button.title} button={button} />
-      ))}
+      {buttons
+        .filter((button) => button.vertical)
+        .map((button) => (
+          <ButtonGridItem key={button.title} button={button} />
+        ))}
     </div>
   </div>
 );
